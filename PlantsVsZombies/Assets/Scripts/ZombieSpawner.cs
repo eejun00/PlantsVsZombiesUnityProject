72fd0b311080990d8dafdf0d7c9b89f5c1392b46
ZEZE\a0104 <a01049469494@gmail.com>
2023-08-08 16:00:33 +0900

    [KJH] feat : 좀비 스포너 수정하여 5개 y값 좌표에 랜덤하게 좀비 생성, 좀비 스포너 프리팹 화

--- PlantsVsZombies/Assets/Scripts/ZombieSpawner.cs
+++ PlantsVsZombies/Assets/Scripts/ZombieSpawner.cs
@@ -1,3 +1,30 @@
+//using System.Collections;
+//using UnityEngine;
+
+//public class ZombieSpawner : MonoBehaviour
+//{
+//    public GameObject zombiePrefab; // 좀비 프리팹
+//    public float spawnInterval = 5f; // 좀비 생성 간격
+
+//    private void Start()
+//    {
+//        // spawnInterval마다 SpawnZombie 함수를 호출
+//        StartCoroutine(SpawnZombieRoutine());
+//    }
+
+//    IEnumerator SpawnZombieRoutine()
+//    {
+//        while (true)
+//        {
+//            // 좀비 프리팹을 현재 스포너 위치에 생성
+//            Instantiate(zombiePrefab, transform.position, Quaternion.identity);
+
+//            // 다음 생성 대기
+//            yield return new WaitForSeconds(spawnInterval);
+//        }
+//    }
+//}
+
 using System.Collections;
 using UnityEngine;
 
@@ -5,6 +32,7 @@ public class ZombieSpawner : MonoBehaviour
 {
     public GameObject zombiePrefab; // 좀비 프리팹
     public float spawnInterval = 5f; // 좀비 생성 간격
+    public float[] allowedYPositions; // 좀비 생성 허용 y좌표 배열
 
     private void Start()
     {
@@ -16,12 +44,20 @@ public class ZombieSpawner : MonoBehaviour
     {
         while (true)
         {
-            // 좀비 프리팹을 현재 스포너 위치에 생성
-            Instantiate(zombiePrefab, transform.position, Quaternion.identity);
+            // 랜덤한 인덱스 선택
+            int randomIndex = Random.Range(0, allowedYPositions.Length);
+
+            // 선택된 인덱스에 해당하는 y좌표 가져오기
+            float spawnYPosition = allowedYPositions[randomIndex];
+
+            // 좀비 생성 위치 설정
+            Vector3 spawnPosition = new Vector3(transform.position.x, spawnYPosition, transform.position.z);
+
+            // 좀비 프리팹을 생성 위치에 생성
+            Instantiate(zombiePrefab, spawnPosition, Quaternion.identity);
 
             // 다음 생성 대기
             yield return new WaitForSeconds(spawnInterval);
         }
     }
 }
-
