817ed2fe3e755b2b27c3e6ca3c4677407ebf3f48
ZEZE\a0104 <a01049469494@gmail.com>
2023-08-09 11:40:35 +0900

    [KJH] feat : 좀비 스크립트 생성, 식물 스크립트 수정

--- PlantsVsZombies/Assets/Scripts/Plant.cs
+++ PlantsVsZombies/Assets/Scripts/Plant.cs
@@ -5,21 +5,25 @@ using UnityEngine;
 public class Plant : MonoBehaviour
 {
     public int cost = 50;
-
+    public float maxHP;                     // 최대 체력
+    private float currentHP;                 // 현재 체력
+    public float MaxHP => maxHP;
+    public float CurrentHP => currentHP;
     private void Awake()
     {
-
+        currentHP = maxHP; // 게임 시작 시 현재 체력을 최대 체력으로 초기화
     }
-
-    void Update()
-    {
-
-
-
-    }
-
     public void Die()
     {
         Destroy(gameObject);
+    }
+    public void TakeDamage(int damage)
+    {
+        currentHP -= damage; // 데미지만큼 체력 감소
+
+        if (currentHP <= 0)
+        {
+            Die(); // 체력이 0 이하면 식물 파괴 처리
+        }
     }
 }
---
+++ PlantsVsZombies/Assets/Scripts/Zombies.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombies : MonoBehaviour
+{
+    [SerializeField]
+    private float maxHP;                     // 최대 체력
+    private float currentHP;                 // 현재 체력
+    private bool isDie = false;              // 적이 사망 상태이면 isDie를 true로 설정
+    public int zombieAD = 1;             // 좀비의 공격 데미지 양
+
+    public float MaxHP => maxHP;
+    public float CurrentHP => currentHP;
+
+    private void Awake()
+    {
+        currentHP = maxHP;   //현재 체력을 최대 체력과 같게 설정
+    }
+
+    public void TakeDamage(float damage)
+    {
+        // Tip. 적의 체력이 damage 만큼 감소해서 죽을 상황일 때 여러 타워의 공격을 동시에 받으면
+        // enemy.OnDie() 함수가 여러 번 실행될 수 있다.
+
+        // 현재 적의 상태가 사망 상태이면 아래 코드를 실행하지 않는다.
+        if (isDie == true) return;
+
+        // 현재 체력을 damage 만큼 감소 
+        currentHP -= damage;
+
+        // 체력이 0이하 = 적 캐릭터 사망
+        if (currentHP <= 0)
+        {
+            isDie = true;
+            // 적 캐릭터 사망
+            //enemy.OnDie(EnemyDestroyType.Kill);
+        }
+    }
+    private void Die()
+    {
+        // 좀비가 사망할 때의 처리, 예를 들어 사망 애니메이션 재생 등을 수행할 수 있음
+        Destroy(gameObject); // 일단 오브젝트 파괴로 처리
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Plant"))
+        {
+            Plant plant = other.GetComponent<Plant>(); // 충돌한 오브젝트의 Plant 컴포넌트 가져오기
+
+            if (plant != null)
+            {
+                plant.TakeDamage(zombieAD); // 데미지 입히기
+            }
+        }
+    }
+}
