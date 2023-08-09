06ddf43163fd6d2eff9ac7f04f0a57626af64e9e
99Leejunyoung <dlwnsdud0612@gmail.com>
2023-08-09 11:40:28 +0900

    [LJY] feat : SunFlower 스크립트에 햇빛 생산 기능 추가, Wallnut 스크립트 기본틀 완성

--- PlantsVsZombies/Assets/Scripts/Plants/SunFlower.cs
+++ PlantsVsZombies/Assets/Scripts/Plants/SunFlower.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class SunFlower : Plant
 {
+    public GameObject sunPrefab;
+
+    private float sunSpawnSpeed = 5f;   // 햇빛 생산 스피드
+    private float sunRespawnTime = default; //햇빛 생산 후의 시간
+    private Transform sunSpawnPosition = null;
+
+    private Vector2 lastPosition;
+    private float timeSinceLastMove = 0f;
+    private float maxIdleTime = 1f; // 1초
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sunSpawnPosition = gameObject.FindChildObject("Head").transform;
+        sunSpawnPosition.position = new Vector3(sunSpawnPosition.position.x, sunSpawnPosition.position.y, 1f); 
+        lastPosition = transform.position;
+        sunRespawnTime = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // 현재 위치와 이전 위치를 비교하여 이동했는지 검사
+        if (Vector2.Distance(transform.position, lastPosition) > 0.1f)
+        {
+            timeSinceLastMove = 0f; // 이동이 감지되면 시간을 초기화
+        }
+        else
+        {
+            timeSinceLastMove += Time.deltaTime; // 이동이 없으면 시간 증가
+        }
+
+        lastPosition = transform.position;
+
+        // 1초 이상 움직이지 않았을 경우 특정 동작 실행
+        if (timeSinceLastMove >= maxIdleTime)
+        {
+            sunRespawnTime += Time.deltaTime;
+            if(sunRespawnTime > sunSpawnSpeed)
+            {
+                sunRespawnTime = 0f;
+                GameObject sun = Instantiate(sunPrefab, sunSpawnPosition.position, transform.rotation);                
+            }
+        }
     }
 }
--- PlantsVsZombies/Assets/Scripts/Plants/Wallnut.cs
+++ PlantsVsZombies/Assets/Scripts/Plants/Wallnut.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Wallnut : Plant
 {
+    private GameObject fullHpImg;
+    private GameObject halfHpImg;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        fullHpImg = gameObject.FindChildObject("FullHpWallnut");
+        halfHpImg = gameObject.FindChildObject("HalfWallnut");
     }
 
     // Update is called once per frame
