571e440410b16a2485f8217c21c63ef44e7c7082
99Leejunyoung <dlwnsdud0612@gmail.com>
2023-08-24 17:56:13 +0900

    [LJY] feat : 최종 코스트 및 엔딩 씬 수정 / 버그 픽스

--- PlantsVsZombies/Assets/Scripts/Particle/DoPunch.cs
+++ PlantsVsZombies/Assets/Scripts/Particle/DoPunch.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 
 public class DoPunch : MonoBehaviour
 {
+    public int vibrato = 10;
     // Start is called before the first frame update
     void Start()
     {
-        transform.DOPunchScale(new Vector3(0.1f,0.1f,0.1f), 500f);
+        transform.DOPunchScale(new Vector3(0.1f,0.1f,0.1f), 500f, vibrato);
     }
 
     // Update is called once per frame
